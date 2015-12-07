0fcd9633b94689fe7d9f8eb765b4ccb9d4f1b6f4
rix1 <rikardeide@gmail.com>
2015-12-07 18:12:43 +0100

    updted textArea

--- client/app.js
+++ client/app.js
@@ -104,12 +104,12 @@ myApp.controller('homeCtrl', ['$scope', 'Content', function($scope, Content) {
 	var generateCSS = function () {
 		$scope.generated_css = "";
 		var cssTemplate = " \
-		// Color #{0} \n\
-		background: -webkit-linear-gradient(left, {1}, {2}); /* For Safari 5.1 to 6.0 */ \n\
-		background: -o-linear-gradient(left, {1}, {2}); /* For Opera 11.1 to 12.0 */ \n\
-		background: -moz-linear-gradient(left, {1}, {2}); /* For Firefox 3.6 to 15 */ \n\
-		background: linear-gradient(left, {1}, {2}); /* Standard syntax */ \n\
-		";
+// Color #{0} \n\
+background: -webkit-linear-gradient(left, {1}, {2}); /* For Safari 5.1 to 6.0 */ \n\
+background: -o-linear-gradient(left, {1}, {2}); /* For Opera 11.1 to 12.0 */ \n\
+background: -moz-linear-gradient(left, {1}, {2}); /* For Firefox 3.6 to 15 */ \n\
+background: linear-gradient(left, {1}, {2}); /* Standard syntax */ \n\
+";
 		var counter = 0;
 		for (var key in exportGradients) {
 			if (exportGradients.hasOwnProperty(key)) {
